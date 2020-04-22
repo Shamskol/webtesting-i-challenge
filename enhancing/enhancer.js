b7b430fc01c4aedc877e1ba9192affab505a69b7
Shamskol <koladejun@yahoo.com>
2020-04-22 23:31:18 +0100

    Modified the enhancer.js and enhancer.spec.js files

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,10 +6,26 @@ module.exports = {
 };
 
 function succeed(item) {
+  let enhancement = item.enhancement
+  if (enhancement < 20){
+    ++enhancement
+  }
+
   return { ...item };
 }
 
 function fail(item) {
+  let enhancement = item.enhancement;
+  let durability = item.durability;
+  if (enhancement < 15) {
+    durability = durability - 5;
+  } else {
+    durability = durability - 10
+    if (enhancement >= 17) {
+      --enhancement;
+    }
+  } 
+
   return { ...item };
 }
 
